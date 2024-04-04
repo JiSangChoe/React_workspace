7d7b41c8fdb30495b5735c7b294efba7c9506bc7
JiSangChoe <chlwltkd123@gmail.com>
2024-04-04 12:03:46 +0900

    2024.04.04 Properties 실습 <SignInContainer />

--- src/Component_manage/Properties.tsx
+++ src/Component_manage/Properties.tsx
@@ -8,18 +8,31 @@ import React from 'react'
 interface Props {
   title: string;
   content: string;
+  // 속성을 선택적으로 사용하기 위해 ?사용
+  nickname?: string;
 }
-function Child(props:Props) {
+function Child({title, content, nickname ='비공개'}: Props) {
   
+  // 1.
   // props = {
   //   title: '제목1',
   //   content: '내용1'
   // }
 
+  // 2.
+  // 이렇게 하면 return 안에 props.title 대신에 title만 써도됨
+  // const title = props.title;
+  // const content = props.content;
+
+  // 3.
+  // 구조의 파괴 디스트럭처 이거기 떄문에 Child의 매개변수에 props 대신에 {title, content}를 넣어도됨
+  // const {title, content} = props;
+
   return (
     <div>
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
+      <h1>{title}</h1>
+      <h4>{nickname}</h4>
+      <p>{content}</p>
     </div>
   )
 }
@@ -27,9 +40,9 @@ function Child(props:Props) {
 export default function Properties() {
   return (
     <>
-      <Child title='제목1' content='내용1'/>
-      <Child />
-      <Child />
+      <Child title='제목1' content='내용1' nickname='로제'/>
+      <Child title='국제인구 이동' content='보도자료' />
+      <Child title='외국인 지열별 통계' content='제가 못찾는 것일까요?'/>
     </>
   )
 }
