19f372541d7df24219b4f64146107d7a2588e3d7
JiSangChoe <chlwltkd123@gmail.com>
2024-04-09 11:52:00 +0900

    2024.04.09.11:50 estate 웹 서비스/ "baseUrl": "./src" 하는방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+                          // tsconfig.json 에서 "baseUrl": "./src"를 추가하면 상대경로로 잡히지 않게 할 수 있다. 모듈화를 더 깔끔하게 함
+import Authentication from 'views/Authentication';
 
 function App() {
   return (
-    <></>
+    <>
+      <Authentication />
+    </>
   );
 }
 
