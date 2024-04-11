87a25e5dd47df2907b7cc309310cf6a6a655b37a
JiSangChoe <chlwltkd123@gmail.com>
2024-04-11 11:51:14 +0900

    2024.04.11.11:50 SnsContainerProps 버튼 만들고, 중복확인

--- src/views/Authentication/index.tsx
+++ src/views/Authentication/index.tsx
@@ -8,6 +8,29 @@ import InputBox from 'components/Inputbox';
 // 타입을 만든거임
 type AuthPage = 'sign-in' | 'sign-up';
 
+interface SnsContainerProps {
+    title: string;
+}
+
+function SnsContainer({title}: SnsContainerProps) {
+
+    // 버튼을 클릭했을때 반응 -> 똑같은 구현부를 가지고 있어서 하나로 묶음 (kakao, naver) -> Sns
+    const onSnsButtonClickHandler = (type: 'kakao' | 'naver') => {
+        alert(type);
+    };
+
+    return (
+        <div className='authentication-sns-container'>
+            <div className='sns-container-title label'>{title}</div>
+            <div className='sns-button-container'>             {/*익명함수로 매개변수없이 호출 */}
+                <div className='sns-button kakao-button' onClick={() => onSnsButtonClickHandler('kakao')}></div>
+                <div className='sns-button naver-button' onClick={() => onSnsButtonClickHandler('naver')}></div>
+            </div>
+        </div>
+    );
+
+};
+
 interface Props {
     onLinkClickHandler: () => void 
 }
@@ -43,22 +66,34 @@ function SignIn ({onLinkClickHandler}: Props) {
                 <div className='text-link' onClick={onLinkClickHandler}>회원가입</div>
             </div>
             <div className='short-divider'></div>
-            <div className='authentication-sns-container'></div>
+            <SnsContainer title="SNS 로그인"/>
         </div>
     );
 }
 
 function SignUp ({onLinkClickHandler}: Props) {
+    // InputBoxProps의 onChangeHandler 상태 만들어줌
+    const [id, setId] = useState<string>('');
+    const [idButtonStatus, setIdButtonStatus] = useState<boolean>(false);
 
+    // 중복확인 버튼을 작동하기 위해 만든 코드인데 하
+    const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target;
+        setId(value);
+        setIdButtonStatus(value !== '');
+    };
+    
     const onSignUpButtonClickHandler = () => {
 
     }
 
     return (
         <div className='authentication-contents'>
-            <div className='authentication-sns-container'></div>
+            <SnsContainer title="SNS 회원가입"/>
             <div className='short-divider'></div>
-            <div className='authentication-input-container'></div>
+            <div className='authentication-input-container'>
+                <InputBox label='아이디' type='text' value={id} placeholder='아이디를 입력해주세요.' onChangeHandler={onIdChangeHandler} buttonTitle='중복확인' buttonStatus={idButtonStatus}/>
+            </div>
             <div className='authentication-button-container'>
                 <div className='primary-button full-width' onClick={onSignUpButtonClickHandler}>회원가입</div>
                 <div className='text-link' onClick={onLinkClickHandler}>로그인</div>
