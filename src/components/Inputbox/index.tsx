ac27c52bc28700689b561599e2e782716f1faf69
JiSangChoe <chlwltkd123@gmail.com>
2024-04-12 11:05:08 +0900

    2024.04.12.10:50 중복확인, 메세지, 에러 상태 만듬// 패스워드만듬

--- src/components/Inputbox/index.tsx
+++ src/components/Inputbox/index.tsx
@@ -10,11 +10,15 @@ export interface InputBoxProps {
     buttonTitle?: string;
     buttonStatus?: boolean;
     onButtonClickHandler?: () => void;
+    message?: string;
+    error?: boolean;
 }
 
-export default function InputBox({label, type, value, placeholder,onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler}:InputBoxProps) {
+export default function InputBox({label, type, value, placeholder,onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler, message, error}:InputBoxProps) {
 
     const buttonClass = buttonStatus ? 'input-primary-button' : 'input-disable-button';
+    // 'input-message ' 이거 왜 띄어쓰기 해야함?
+    const messageClass = 'input-message ' + (error ? 'error' : 'primary');
 
     return (
         <div className="input-box">
@@ -25,7 +29,9 @@ export default function InputBox({label, type, value, placeholder,onChangeHandle
                         <div  className={buttonClass} onClick={onButtonClickHandler}>{buttonTitle}</div> 
                         }
                 </div>
-            <div className="input-message"></div>
+            <div className={messageClass}>
+                {message}
+            </div>
         </div>
     );
 }
