febc050974149125e8e2348a1ccf8cb6becd7785
JiSangChoe <chlwltkd123@gmail.com>
2024-04-18 11:53:43 +0900

    2024.04.18.11:50 axios 패키지/  URL, option을 매개변수

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
 
       <Route path='/router' element={<Layout />}>
         <Route index element={<ReactRouterLibrary/>} />
-        <Route path='page1' element={<ReactRouterPage1 />} />
+        <Route path='page1/:value' element={<ReactRouterPage1 />} />
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
+      <Route path='/axios' element={<AxiosLibrary/>} />
       <Route path='*' element={<h3>404 Error</h3>} />
     </Routes>
   );
